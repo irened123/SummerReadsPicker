6ec0101b03ce3a52f2d920f6a16835da368413bc
irened123 <irenedeng7@gmail.com>
2024-05-28 23:05:08 -0400

    Add data structures for genre tracking, book recommendations, and wishlist

---
+++ data.py
@@ -0,0 +1,31 @@
+genres = ["detective fiction", "historical fiction", "historical nonfiction", "romance", "science fiction"]
+book_recommendations = {
+    "detective fiction": {
+        "And Then There Were None": {"author": "Agatha Christie", "rating": "4.5/5 (1,689 ratings)", "keywords": ["island", "strangers", "survival"]},
+        "Murder on the Orient Express": {"author": "Agatha Christie", "rating": "4/5 (1,078 ratings)", "keywords": ["train travel", "plot twist", "Hercule Poirot"]},
+        "The Last Good Kiss": {"author": "James Crumley", "rating": "3.5/5 (39 ratings)", "keywords": ["love", "alcoholism", "American West"]}
+    },
+    "historical fiction": {
+        "Homegoing": {"author": "Yaa Gyasi", "rating": "4.5/5 (1,911 ratings)", "keywords": ["diaspora", "colonialism", "family saga"]},
+        "World Without End": {"author": "Ken Follett", "rating": "4.5/5 (553 ratings)", "keywords": ["Black Death", "medieval England", "love"]},
+        "A Tale of Two Cities": {"author": "Charles Dickens", "rating": "4/5 (526 ratings)", "keywords": ["French Revolution", "corruption", "love"]}
+    },
+    "historical nonfiction": {
+        "The Devil in the White City": {"author": "Erik Larson", "rating": "4/5 (1,796 ratings)", "keywords": ["Gilded Age", "serial killer", "19th century Chicago"]},
+        "Under the Banner of Heaven": {"author": "Jon Krakauer", "rating": "4/5 (548 ratings)", "keywords": ["violent religion", "true crime", "culture clash"]},
+        "The Indifferent Stars Above": {"author": "Daniel Brown", "rating": "4.5/5 (164 ratings)", "keywords": ["westward migration", "survival", "tragedy"]}
+    },
+    "romance": {
+        "Pride and Prejudice": {"author": "Jane Austen", "rating": "4.5/5 (1,884 ratings)", "keywords": ["19th century England", "marriage", "gender roles"]},
+        "Gone With the Wind": {"author": "Margaret Mitchell", "rating": "4.5/5 (623 ratings)", "keywords": ["Civil War", "racial dynamics", "plantation life"]},
+        "The Notebook": {"author": "Nicholas Sparks", "rating": "4/5 (716 ratings)", "keywords": ["family dynamics", "second chances", "class differences"]}
+    },
+     "science fiction": {
+        "Dune": {"author": "Frank Herbert", "rating": "4.5/5 (1,719 ratings)", "keywords": ["desert planet", "religion", "hero's journey"]},
+        "The Sirens of Titan": {"author": "Kurt Vonnegut", "rating": "4/5 (312 ratings)", "keywords": ["satire", "space", "free will/fate"]},
+        "Childhood's End": {"author": "Arthur Charles", "rating": "4/5 (267 ratings)", "keywords": ["utopia", "alien invasion", "telepathy"]}
+    }     
+}
+wish_list = {
+    
+}

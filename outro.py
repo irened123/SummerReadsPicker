cfcb5216d1482e7620b71599731d3ae1c7e28ee5
irened123 <irenedeng7@gmail.com>
2024-05-30 22:03:09 -0400

    Add function printing outro

---
+++ outro.py
@@ -0,0 +1,15 @@
+jgs_ascii_art = """
+          |
+        \ _ /
+      -= (_) =-
+        /   \         _\/_
+          |           //o\  _\/_
+   _____ _ __ __ ____ _ | __/o\\ _
+ =-=-_-__=_-= _=_=-=_,-'|"'""-|-,_
+  =- _=-=- -_=-=_,-"          |
+jgs =- =- -=.--"
+"""
+
+def print_outro():
+    print("\nGreat job building your wishlist! Happy reading, and see you next time! :)")
+    print(jgs_ascii_art)

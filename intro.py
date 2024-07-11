b871269454de3d20621ffe96e9cd28b6c68f0750
irened123 <irenedeng7@gmail.com>
2024-07-10 23:46:54 -0400

    Remove ASCII images from intro.py and outro.py

--- intro.py
+++ intro.py
@@ -1,4 +1,3 @@
-
 def print_intro():
     print("Hi there! Ready to build the perfect reading wishlist for this summer?") 
   
--- outro.py
+++ outro.py
@@ -1,3 +1,2 @@
-
 def print_outro():
     print("\nGreat job building your wishlist! Happy reading, and see you next time! :)")
